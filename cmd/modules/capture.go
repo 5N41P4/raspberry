995481ae9a78944c315615190e2c52f3e45cd52c
Aurel Corti <aurel.corti@gmail.com>
2024-05-03 13:54:38 +0200

    Changes in Capture

--- cmd/modules/capture.go
+++ cmd/modules/capture.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"time"
@@ -11,14 +12,29 @@ func (i *Interface) captureStart() error {
 	mon := exec.Command("sudo", "airmon-ng", "start", i.Name)
 	_ = mon.Run()
 
-	dir := exec.Command("sudo", "mkdir", "/usr/local/raspberry/captures/")
-	_ = dir.Run()
+	fileInfo, err := os.Stat("/usr/local/raspberry/captures")
+
+	if os.IsNotExist(err) || !fileInfo.IsDir() {
+		err = os.MkdirAll("/usr/local/raspberry/captures", 0770)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	i.State = "capture"
 
 	// Create a Capture and an ID
 	id := i.Name + time.Now().Format("20060102150405")
 
-	cmd := exec.Command("sudo", "airodump-ng", "-K", "1", "--write", "usr/local/raspberry/captures/"+id, "--output-format", "cap", "--wps", i.Name)
-	err := cmd.Run()
+	i.process = exec.Command("sudo", "airodump-ng", "-K", "1", "--write", "/usr/local/raspberry/captures/"+id, "--output-format", "cap", "--wps", i.Name)
+	err = i.process.Run()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	i.State = "up"
 
 	return err
 }
